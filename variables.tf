9120fa6e463ad7141bd6bc9897d56ab4a0984538
kotavssj <kotaven1989@gmail.com>
2025-09-01 10:29:22 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,14 @@
-
+#01/09/2025
+#===============
 #Interpolations
 #varibles and tring together
 "${}-hello"
+
+varible "ingress_ports" {
+  default = [ #list(map)
+      {
+      from_port = 22
+      to_port  =  23
+      }
+  ]
+}
