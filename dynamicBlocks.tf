3908aee9c28df24484a4622fc786d09a7b61587c
kotavssj <kotaven1989@gmail.com>
2025-09-01 10:33:11 +0530

    Update dynamicBlocks.tf

--- dynamicBlocks.tf
+++ dynamicBlocks.tf
@@ -1,4 +1,15 @@
-ingress{
+
+#01/09/2025
+dynamic ingress{
+  for_each = var.ingress_ports
+    content{
+      from_port = ingress.value["from_port"]
+      to_port = ingress.value["to_port"]
+      protocol = "-1"
+      cidr_blocks = var.cidr_blocks
+  }
+
+
 from_port = 22
 to_port = 25
 }
